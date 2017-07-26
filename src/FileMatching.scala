86138a61b65a9026151da2e685cf9474793eed0c
Naveen <Naveen3665@hotmail.com>
2017-07-26 18:58:26 +0530

    July 26th 2017 Currying, Repeated Functions, Higher order functions

--- src/FileMatching.scala
+++ src/FileMatching.scala
@@ -5,22 +5,44 @@ import java.io.File
   */
 object FileMatching {
 
-  private def fileList = new File("/Users/MacUser/Documents/Documents/Scala Programming/").listFiles()
+  private def fileList = new File("D:\\NaveenFiles\\Scala Programming\\Documentation\\Scala Programming\\scalaBasicTesting").listFiles()
 
+  private val testList : List[Int]= List(1,2,5,3,4,56,7,78,7)
   def fileEnding(query : String) = {
-    val fileEndingFnValue =
-    fileMatching(query,)
+    val fileEndingFnValue = (value1: String) => value1.endsWith(query)
+    fileMatching(fileEndingFnValue)
   }
 
   def fileContaining(query : String) = {
-    for(file <-fileList ; if file.getName.contains(query))
+    fileMatching(_.contains(query))
+  }
+
+  def fileMatching(matcher :(String) => Boolean) = {
+    for(file <- fileList ; if matcher(file.getName))
       yield file
   }
 
-  def fileMatching(query : String, matcher :(String,String) => Boolean) = {
-    for(file <- fileList ; if matcher(file.getName,query))
-      yield file
+
+  def containsImpertiveWay(findNum : Int) : Boolean = {
+    var exist = false
+    for(testNum <- testList) {
+      if(testNum == findNum)
+        exist = true
+    }
+    exist
   }
+
+  def containsFunctionalWay(findNum : Int) : Boolean = testList.exists(_ == findNum)
+
+
+  //Curried Function testing using functional Values
+
+  def normalAdd(x : Int, y : Int) = x+y
+
+  def curriedFunction(x : Int)(y : Int) = x+y
+
+  def spiltCurriedFnValue(x:Int) = (y : Int) => x+y // If we call this method it will return the function value later it can be used to add the values x & y
+
 
 
 }
@@ -31,5 +53,20 @@ object testFileMatching  {
     FileMatching.fileEnding(".scala").toList.foreach(f => println(f.getName))
     println("-----File Containing method check-----")
     FileMatching.fileContaining("java").toList.foreach(f => println(f.getName))
+    println()
+    println("Testing the Higher order functions exist method imperative way")
+    println("3 exist in the list :"+FileMatching.containsImpertiveWay(3))
+    println("4 exist in the list 'Functional way' :"+FileMatching.containsFunctionalWay(4))
+
+    //Curried Function testing
+    println()
+    println("Curried Function")
+    println("Normal additon :"+FileMatching.normalAdd(4,5))
+    println("Curried addition :"+FileMatching.curriedFunction(5)(6))
+    val first = FileMatching.spiltCurriedFnValue(2)
+    println("Adding the curried values using function values :"+first(3))
+    val curriedFirst = FileMatching.curriedFunction(17)_
+    println("Adding the curried values based on partial applied functions:"+curriedFirst(10))
+
   }
 }
